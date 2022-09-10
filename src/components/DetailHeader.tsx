28366aab82c9fcfd656aa28c0b461b1647f21f65
Yextds <Yext@dotsquares.com>
2022-09-10 14:28:50 +0530

    add locator content

--- src/components/DetailHeader.tsx
+++ src/components/DetailHeader.tsx
@@ -43,7 +43,7 @@ const links: Link[] = [
   },
   {
     label: "LOCATION",
-    url: "/turtlehead-tacos",
+    url: "https://master-lurch--padding--anemia-sbx-pgsdemo-com.sbx.preview.pagescdn.com/turtlehead-tacos",
   },
 ];
 
